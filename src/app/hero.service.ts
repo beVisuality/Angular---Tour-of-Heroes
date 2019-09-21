d3fed593056eddeae522147c7b4705fcf7e44e8b
Zoltán Beviz <bevisuality@gmail.com>
2019-09-21 14:55:58 +0200

    Add update hero name function

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -9,6 +9,10 @@ import { catchError, map, tap } from 'rxjs/Operators';
   providedIn: 'root'
 })
 export class HeroService {
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
   private heroesUrl = 'api/heroes';  // URL to web api
 
   constructor(
@@ -32,18 +36,26 @@ export class HeroService {
     )
   }
 
+  /** PUT: update the hero on the server */
+  updateHero (hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`update hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
+  }
+
   /** Log a HeroService message with the MessageService */
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
   }
 
   /**
- * Handle Http operation that failed.
- * Let the app continue.
- * @param operation - name of the operation that failed
- * @param result - optional value to return as the observable result
- */
-private handleError<T> (operation = 'operation', result?: T) {
+  * Handle Http operation that failed.
+  * Let the app continue.
+  * @param operation - name of the operation that failed
+  * @param result - optional value to return as the observable result
+  */
+  private handleError<T> (operation = 'operation', result?: T) {
   return (error: any): Observable<T> => {
 
     // TODO: send the error to remote logging infrastructure
@@ -54,6 +66,6 @@ private handleError<T> (operation = 'operation', result?: T) {
 
     // Let the app keep running by returning an empty result.
     return of(result as T);
-  };
+  }
 }
 }
